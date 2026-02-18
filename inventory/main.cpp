16ec5b0a4318285054cee4dfc10d78f61f88f940
stolec <konto.anonimowe.xx@gmail.com>
2026-02-18 22:47:13 +0100

    Refactor main function for platform-specific handling

--- inventory/main.cpp
+++ inventory/main.cpp
@@ -2,27 +2,18 @@
 #include "game.h"
 #ifdef _WIN32
 #include "windows.h"
-
-
-
-int main() {
-    // std::cout << "Detected OS: " << OS_NAME << std::endl;
-    SetConsoleOutputCP(65001); //Sets console to UTF-8 mode to avoid issues with map
-
-    Game game;
-    game.setPlayerName();
-    game.play();
-    // game.getPlayer()->getInv()->getItemOnSelectedRC(game.getPlayer()->getInv()->getCurrentRow(), game.getPlayer()->getInv()->getCurrentCol()).
-
-    return 0;
-}
-#elif _APPLE
-int main() {
-    Game game;
-    game.setPlayerName();
-    game.play();
-    // game.getPlayer()->getInv()->getItemOnSelectedRC(game.getPlayer()->getInv()->getCurrentRow(), game.getPlayer()->getInv()->getCurrentCol()).
-
-    return 0;
-}
 #endif
+
+
+int main() {
+#ifdef _WIN32
+    SetConsoleOutputCP(65001); //Sets console to UTF-8 mode to avoid issues with map
+#endif
+    Game game;
+    game.setPlayerName();
+    game.play();
+
+    return 0;
+}
+
+
